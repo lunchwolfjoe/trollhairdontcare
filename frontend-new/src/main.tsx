20c78476adb927576f9c37ae30b98303f3e60543
lunchwolfjoe <nyk_plumb@yahoo.com>
2025-04-21 12:04:50 -0500

    Fix App import in main.tsx to use default import

--- frontend-new/src/main.tsx
+++ frontend-new/src/main.tsx
@@ -1,7 +1,7 @@
 import 'leaflet/dist/leaflet.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
+import App from './App.tsx';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
